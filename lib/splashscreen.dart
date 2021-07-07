3566656ae6008c8e381374cdda095f31e03887f3
kalfinbagas <86760581+kalfinbagas@users.noreply.github.com>
2021-07-07 16:50:53 +0700

    Feature: MM-12 - Splash screen and unfinished login page

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+
 import 'package:macman/home.dart';
+import 'package:macman/loginpage.dart';
 
 class SplashScreen extends StatefulWidget {
   _SplashScreen createState() => _SplashScreen();
@@ -18,33 +20,37 @@ class _SplashScreen extends State<SplashScreen> {
     return Timer(duration, () {
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => Home()),
+        MaterialPageRoute(builder: (context) => LoginPage()),
       );
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          stops: [0.5, 0, 5],
-          colors: <Color>[Color(0x2CF4A3), Color(0xBE187)],
-          tileMode: TileMode.repeated,
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Image.asset(
-            'img/biglogo.png',
-            width: 200.0,
-            height: 100.0,
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            // stops: [0.5, 0, 5],
+            colors: <Color>[Color(0xFF2CF4A3), Color.fromRGBO(11, 225, 135, 1)],
+            // tileMode: TileMode.repeated,
           ),
-        ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'img/biglogo.png',
+              width: 200.0,
+              height: 100.0,
+            ),
+          ],
+        ),
       ),
     );
   }
